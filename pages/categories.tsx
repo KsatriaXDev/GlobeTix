1fc23f2d3ab5a1834d822df4922bdff8ee4c1aa5
Ksatria Pamungkas <ksatriaxdev@gmail.com>
2023-07-20 19:26:03 +0700

    add features

--- pages/categories.tsx
+++ pages/categories.tsx
@@ -2,6 +2,8 @@ import Link from "next/link";
 import React from "react";
 import Hero from "./components/molecules/Hero";
 import Top from "./components/molecules/Top";
+import Discount from "./components/molecules/Discount";
+import New from "./components/molecules/New";
 
 export default function categories() {
   return (
@@ -47,68 +49,7 @@ export default function categories() {
 
       <Top/>
 
-      <div className="container px-4 mx-auto my-16 md:px-12">
-        <h2 className="mb-4 text-xl font-medium md:mb-0 md:text-lg">Discount</h2>
-        <div className="flex flex-wrap -mx-1 lg:-mx-4">
-          <div className="w-full px-1 my-1 md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
-            <div className="overflow-hidden border border-gray-200 rounded-xl">
-              <Link href="/details">
-                <div className="m-4 overflow-hidden rounded-xl">
-                  <img alt="Placeholder" className="block w-full h-auto" src="/img/item/Monas1.jpg" />
-                </div>
-              </Link>
-              <header className="px-4 mb-4 leading-tight">
-                <h1 className="text-lg">
-                  <a className="font-semibold text-black no-underline hover:underline" href="monas.html">
-                    Monumen Nasional, DKI Jakarta
-                  </a>
-                </h1>
-                <span className="block text-sm font-light text-gray-500 no-underline">
-                  DKI Jakarta
-                </span>
-              </header>
-            </div>
-          </div>
-          <div className="w-full px-1 my-1 md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
-            <div className="overflow-hidden border border-gray-200 rounded-xl">
-              <Link href="/details">
-                <div className="m-4 overflow-hidden rounded-xl">
-                  <img alt="Placeholder" className="block w-full h-auto" src="img/items-2.jpg" />
-                </div>
-              </Link>
-              <header className="px-4 mb-4 leading-tight">
-                <h1 className="text-lg">
-                  <a className="font-semibold text-black no-underline hover:underline" href="#">
-                    Online Doctor Consultation
-                  </a>
-                </h1>
-                <span className="block text-sm font-light text-gray-500 no-underline">
-                  Website UI Kit
-                </span>
-              </header>
-            </div>
-          </div>
-          <div className="w-full px-1 my-1 md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
-            <div className="overflow-hidden border border-gray-200 rounded-xl">
-              <Link href="/details">
-                <div className="m-4 overflow-hidden rounded-xl">
-                  <img alt="Placeholder" className="block w-full h-auto" src="img/items-3.jpg" />
-                </div>
-              </Link>
-              <header className="px-4 mb-4 leading-tight">
-                <h1 className="text-lg">
-                  <a className="font-semibold text-black no-underline hover:underline" href="#">
-                    Banking Crypto
-                  </a>
-                </h1>
-                <span className="block text-sm font-light text-gray-500 no-underline">
-                  Mobile UI Kit
-                </span>
-              </header>
-            </div>
-          </div>
-        </div>
-      </div>
+    <Discount/>
 
       {/* <div className="container px-4 mx-auto my-16 md:px-12">
         <div id="controls-carousel" className="relative w-full" data-carousel="static">
@@ -160,167 +101,7 @@ export default function categories() {
         </div>
       </div> */}
 
-      <div className="container px-4 mx-auto my-16 md:px-12">
-        <h2 className="mb-4 text-xl font-medium md:mb-0 md:text-lg">New Destination</h2>
-        <div className="flex flex-wrap -mx-1 lg:-mx-4 review-slider">
-          <div className="w-full px-1 my-1 md:w-1/2 lg:my-4 lg:px-4 lg:w-1/4">
-            <div className="overflow-hidden border border-gray-200 rounded-xl">
-              <a href="monas.html">
-                <div className="m-4 overflow-hidden rounded-xl">
-                  <img alt="Placeholder" className="block w-full h-auto" src="img/categories-3.jpg" />
-                </div>
-              </a>
-              <header className="px-4 mb-4 leading-tight">
-                <h1 className="text-lg">
-                  <a className="font-semibold text-black no-underline hover:underline" href="monas.html">
-                    Monumen Nasional, DKI Jakarta
-                  </a>
-                </h1>
-                <span className="block text-sm font-light text-gray-500 no-underline">
-                  731 items
-                </span>
-              </header>
-            </div>
-          </div>
-          <div className="w-full px-1 my-1 md:w-1/2 lg:my-4 lg:px-4 lg:w-1/4">
-            <div className="overflow-hidden border border-gray-200 rounded-xl">
-              <Link href="/details">
-                <div className="m-4 overflow-hidden rounded-xl">
-                  <img alt="Placeholder" className="block w-full h-auto" src="img/categories-3.jpg" />
-                </div>
-              </Link>
-              <header className="px-4 mb-4 leading-tight">
-                <h1 className="text-lg">
-                  <a className="font-semibold text-black no-underline hover:underline" href="borobudur.html">
-                    Candi Borobudur, Yogyakarta
-                  </a>
-                </h1>
-                <span className="block text-sm font-light text-gray-500 no-underline">
-                  657 items
-                </span>
-              </header>
-            </div>
-          </div>
-          <div className="w-full px-1 my-1 md:w-1/2 lg:my-4 lg:px-4 lg:w-1/4">
-            <div className="overflow-hidden border border-gray-200 rounded-xl">
-              <Link href="/details">
-                <div className="m-4 overflow-hidden rounded-xl">
-                  <img alt="Placeholder" className="block w-full h-auto" src="img/categories-3.jpg" />
-                </div>
-              </Link>
-              <header className="px-4 mb-4 leading-tight">
-                <h1 className="text-lg">
-                  <a className="font-semibold text-black no-underline hover:underline" href="#">
-                    Icon Set
-                  </a>
-                </h1>
-                <span className="block text-sm font-light text-gray-500 no-underline">
-                  83,559 items
-                </span>
-              </header>
-            </div>
-          </div>
-          <div className="w-full px-1 my-1 md:w-1/2 lg:my-4 lg:px-4 lg:w-1/4">
-            <div className="overflow-hidden border border-gray-200 rounded-xl">
-              <Link href="/details">
-                <div className="m-4 overflow-hidden rounded-xl">
-                  <img alt="Placeholder" className="block w-full h-auto" src="img/categories-3.jpg" />
-                </div>
-              </Link>
-              <header className="px-4 mb-4 leading-tight">
-                <h1 className="text-lg">
-                  <a className="font-semibold text-black no-underline hover:underline" href="#">
-                    Icon Set
-                  </a>
-                </h1>
-                <span className="block text-sm font-light text-gray-500 no-underline">
-                  83,559 items
-                </span>
-              </header>
-            </div>
-          </div>
-          <div className="w-full px-1 my-1 md:w-1/2 lg:my-4 lg:px-4 lg:w-1/4">
-            <div className="overflow-hidden border border-gray-200 rounded-xl">
-              <Link href="/details">
-                <div className="m-4 overflow-hidden rounded-xl">
-                  <img alt="Placeholder" className="block w-full h-auto" src="img/categories-4.jpg" />
-                </div>
-              </Link>
-
-              <header className="px-4 mb-4 leading-tight">
-                <h1 className="text-lg">
-                  <a className="font-semibold text-black no-underline hover:underline" href="#">
-                    Website UI Kit
-                  </a>
-                </h1>
-                <span className="block text-sm font-light text-gray-500 no-underline">
-                  4,500 items
-                </span>
-              </header>
-            </div>
-          </div>
-          <div className="w-full px-1 my-1 md:w-1/2 lg:my-4 lg:px-4 lg:w-1/4">
-            <div className="overflow-hidden border border-gray-200 rounded-xl">
-              <Link href="/details">
-                <div className="m-4 overflow-hidden rounded-xl">
-                  <img alt="Placeholder" className="block w-full h-auto" src="img/categories-4.jpg" />
-                </div>
-              </Link>
-
-              <header className="px-4 mb-4 leading-tight">
-                <h1 className="text-lg">
-                  <a className="font-semibold text-black no-underline hover:underline" href="#">
-                    Website UI Kit
-                  </a>
-                </h1>
-                <span className="block text-sm font-light text-gray-500 no-underline">
-                  4,500 items
-                </span>
-              </header>
-            </div>
-          </div>
-          <div className="w-full px-1 my-1 md:w-1/2 lg:my-4 lg:px-4 lg:w-1/4">
-            <div className="overflow-hidden border border-gray-200 rounded-xl">
-              <Link href="/details">
-                <div className="m-4 overflow-hidden rounded-xl">
-                  <img alt="Placeholder" className="block w-full h-auto" src="img/categories-4.jpg" />
-                </div>
-              </Link>
-
-              <header className="px-4 mb-4 leading-tight">
-                <h1 className="text-lg">
-                  <a className="font-semibold text-black no-underline hover:underline" href="#">
-                    Website UI Kit
-                  </a>
-                </h1>
-                <span className="block text-sm font-light text-gray-500 no-underline">
-                  4,500 items
-                </span>
-              </header>
-            </div>
-          </div>
-          <div className="w-full px-1 my-1 md:w-1/2 lg:my-4 lg:px-4 lg:w-1/4">
-            <div className="overflow-hidden border border-gray-200 rounded-xl">
-              <Link href="/details">
-                <div className="m-4 overflow-hidden rounded-xl">
-                  <img alt="Placeholder" className="block w-full h-auto" src="img/categories-4.jpg" />
-                </div>
-              </Link>
-
-              <header className="px-4 mb-4 leading-tight">
-                <h1 className="text-lg">
-                  <a className="font-semibold text-black no-underline hover:underline" href="#">
-                    Website UI Kit
-                  </a>
-                </h1>
-                <span className="block text-sm font-light text-gray-500 no-underline">
-                  4,500 items
-                </span>
-              </header>
-            </div>
-          </div>
-        </div>
-      </div>
+      <New/>
     </>
   );
 }
