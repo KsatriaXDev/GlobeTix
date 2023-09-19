53e49efb8d32c580d0b292d754992d729787144f
Reindra Antoni <reindraantoni@gmail.com>
2023-09-19 11:26:34 +0700

    Monas

--- src/app/(app)/java/(details)/layout.tsx
+++ src/app/(app)/java/(details)/layout.tsx
@@ -1,11 +1,14 @@
 import Header from "@/components/Shared/Fragments/Header";
+import MonasProvider from "@/context/MonasContext";
 
 export default function DetailLayout({children}: {children: React.ReactNode}) {
   return (
     <div>
       <Header />
-      <div className="mt-20 px-10">
-        {children}
+      <div className="pt-20 px-10 bg-[#f5f5f5]">
+        <MonasProvider>
+          {children}
+        </MonasProvider>
       </div>
       
     </div>
--- src/components/Shared/Fragments/Header.tsx
+++ src/components/Shared/Fragments/Header.tsx
@@ -11,7 +11,7 @@ type Props = {
 const Navbar = ({top, discount, newly}: Props) => {
   const {userId} = auth();
   return (
-    <nav className="fixed z-50 top-0 w-full bg-white border-gray-200 px-2 sm:px-4 py-2.5 shadow">
+    <nav className="fixed z-50 top-0 w-full bg-white border-gray-200 px-2 sm:px-4 py-2.5 shadow-md">
       <div className="container flex flex-wrap items-center justify-between mx-auto my-2">
         <Link href="/" className="flex items-center h-full">
           <div className="dark:text-white text-xl">
