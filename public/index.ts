9c7f29a5e59a8064acf4399449d63a5bc7a2fbf9
Reindra Antoni <reindraantoni@gmail.com>
2023-09-24 11:09:28 +0700

    Hero bali

--- public/index.ts
+++ public/index.ts
@@ -1,4 +1,4 @@
 import HomeImg from "./images/homeimg.jpeg";
 import JavaHero from "./images/Java-Island.jpeg";
-
-export {HomeImg, JavaHero};
+import BaliHeroImg from "./images/hero bali.png";
+export {HomeImg, JavaHero, BaliHeroImg};
--- src/components/Bali/Elements/HeroImage.tsx
+++ src/components/Bali/Elements/HeroImage.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import Image from "next/image";
-import {JavaHero} from "../../../../public";
+import {BaliHeroImg} from "../../../../public";
 
 type Props = {};
 
 const HeroImage = (props: Props) => {
   return (
     <div className="flex justify-center items-center h-full">
-      <Image className="w-[600px] h-[700px] rounded-lg" src={JavaHero} alt="bali-hero" />
+      <Image className="w-[600px] h-[700px] rounded-lg" src={BaliHeroImg} alt="bali-hero" />
     </div>
   );
 };
