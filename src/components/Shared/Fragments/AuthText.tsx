107c932a2910e40fc46dafed8a6b267512f9415c
Reindra Antoni <reindraantoni@gmail.com>
2023-09-24 12:33:33 +0700

    AuthText

--- src/components/Shared/Fragments/AuthText.tsx
+++ src/components/Shared/Fragments/AuthText.tsx
@@ -6,9 +6,9 @@ const AuthText = (props: Props) => {
   return (
     <div className="hidden lg:inline-flex flex-col gap-10">
       <p className="text-3xl font-bold">Globe<span className="text-blue-500">Tix</span></p>
-      <p className="w-2/3 text-lg font-semibold">
-        Follow your dream to travel the world with us. GlobeTix are here to help you every step of the way.
-      </p>
+      <h1 className="text-xl w-2/3 font-bold leading-normal tracking-tight text-gray-900 sm:text-2xl dark:text-white dark:bg-gray-900">
+        Discover. Travel. Repeat: Your Trusted Partner for Seamless Ticketing to the Indonesia Most Enchanting Destinations and Experiences.
+        </h1>
     </div>
   );
 };
